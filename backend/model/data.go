f56967b6858ccaaa25e48c8b7ae38f3b2acbb4ca
Benjaminyuan <1459477412@qq.com>
2019-06-09 06:00:49 +0800

    fix a bug

---
+++ backend/model/data.go
@@ -0,0 +1,22 @@
+package model 
+type News struct {
+	Multi string `json:"multi"`
+	Text string `json:"text"`
+	User User `json:"user"`
+	HasUrl bool `json:"has_url"`
+	Comments int `json:"comments"`
+	Pics int `json:"pics"`
+	Source string `json:"source"`
+	Likes int `json:"likes"`
+	Time int `json:"time"`
+	Reposts int `json:"reposts"`
+}
+type User struct {
+	Verified bool `json:"verified"`
+	Description bool `json:"description"`
+	Gender string `json:"gender"`
+	Messages int  `json:"messages"`
+	Followers int `json:"followers"`
+	Location string `json:"location"`
+	VerifiedType int `json:"verified_type"`
+}
